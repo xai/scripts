c229bed677b4e8c2b9274ab26cb71f0ddfbefaf5
Olaf Lessenich <lessenic@fim.uni-passau.de>
2016-06-02 17:25:18 +0200

    Add multiple thresholds
    Signed-off-by: Olaf Lessenich <lessenic@fim.uni-passau.de>

--- wasserpegel.sh
+++ wasserpegel.sh
@@ -3,15 +3,30 @@
 # Setup as a cronjob: wasserpegel.sh <mail adresses>
 
 # This should give us enough time; shit hits the fan at around 900 (source: 2013).
-THRESHOLD=700
+THRESHOLD1=600
+THRESHOLD2=700
+THRESHOLD3=800
 
 line=$(wget -O - 'http://www.hnd.bayern.de/pegel/donau_bis_passau/passau-18009000' 2>/dev/null | grep -a 'Letzter Messwert vom' | sed -e 's/.*<b>\(.*\)<\/b>.*<b>\(.*\)<\/b>.*/\1 \2/')
+
 if [ -z $1 ]; then
+	# just print current status
 	echo $line
 	exit 0
 fi
-MSG="$(echo $line | awk '{if($3>='$THRESHOLD') { print $0 }}')"
+
+level=$(echo $line | awk '{ print $3 }')
+MSG=""
+
+if (( "$level" >= $THRESHOLD3 )); then
+	MSG="You are in serious trouble really soon! Hurry!"
+elif (( "$level" >= $THRESHOLD2 )); then
+	MSG="Get yourself ready to evacuate our servers!"
+elif (( "$level" >= $THRESHOLD1 )); then
+	MSG="Tide is quite high, let's hope for the best!"
+fi
+
 if [ ! -z "$MSG" ]; then
-	echo -e "Water line Inn:\n$MSG\n\nGet yourself ready to evacuate our servers!" | mail -s "[high tide auto-warning] Inn hits $THRESHOLD cm." $@
+	echo -e "${MSG}\n\nWater line Inn:\n$line" | mail -s "[high tide auto-warning] Inn hits $THRESHOLD cm." $@
 fi
 
