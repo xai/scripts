93fb8dbea2d770f45c46bce8ebd693fa8e2e1359
Olaf Lessenich <xai@linux.com>
2021-02-03 21:40:57 +0100

    spamcheck: use headers instead of log for lookup

--- spamcheck_new_mails.sh
+++ spamcheck_new_mails.sh
@@ -6,12 +6,12 @@ LOGFILE="${HOME}/.spamlog"
 lockfile -r 0 ${LOCKFILE} || exit 0
 
 #inboxes="$(find ~/.mail -type d -name INBOX)"
-inboxes="/home/xai/.mail/wu/olesseni/INBOX"
+inboxes="$HOME/.mail/wu/olesseni/INBOX $HOME/.mail/fim/lessenic/INBOX"
 
 for inbox in $inboxes; do
 	for msg in $(find ${inbox}/new -type f); do
 		id="$(egrep -m1 '^Message-ID:' $msg | sha1sum | cut -d' ' -f1)"
-		if [ ! -f $LOGFILE ] || [ "$id" != "" ] && ! grep -q "$id" ~/.spamlog; then
+		if [ ! -f $LOGFILE ] || [ "$id" != "" ] && ! grep -q 'X-Spam-Status: Yes' $msg; then
 			spamcheck ${msg} >> $LOGFILE
 		fi
 	done
