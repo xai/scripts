8b2c4d4aa9526c132d4ce2280d52a1a8bd690132
Olaf Lessenich <xai@linux.com>
2024-02-08 01:33:48 +0100

    disable tracker script

---
+++ disable-tracker.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+systemctl --user mask tracker-extract-3.service tracker-miner-fs-3.service tracker-miner-rss-3.service tracker-writeback-3.service tracker-xdg-portal-3.service tracker-miner-fs-control-3.service
+tracker3 reset -s -r
