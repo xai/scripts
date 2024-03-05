9c14635b4633c2a9d94060871cbee873f80d3422
Olaf Lessenich <xai@linux.com>
2024-03-05 18:05:56 +0100

    fix: gpg-reencrypt must support multiple keys

--- gpg-reencrypt.sh
+++ gpg-reencrypt.sh
@@ -1,68 +1,102 @@
 #!/bin/bash
 
-# Usage: gpg-reencrypt.sh <old-pub-key> <new-pub-key> <file1> [<file2> ...]
-
 usage() {
-    echo "Usage: gpg-reencrypt.sh [--dry-run] old-ssb old- <file1> [<file2> ...]"
+    echo "Usage: gpg-reencrypt.sh [--dry-run] -o old-key-id -n new-key-id [-n another-new-key-id] <file1> [<file2> ...]"
     exit 1
 }
 
-# -n is an alias for --dry-run
-if [ "$1" = "--dry-run" ] || [ "$1" = "-n" ]; then
-	DRY_RUN=1
-	shift
+old_key_id=""
+new_key_ids=()
+dry_run=0
+files=()
+
+while getopts ":0o:n:" opt; do
+  case ${opt} in
+    o )
+      old_key_id=$OPTARG
+      ;;
+    n )
+      new_key_ids+=("$OPTARG")
+      ;;
+    0 )
+      dry_run=1
+      ;;
+    \? )
+      echo "Invalid option: $OPTARG" 1>&2
+      exit 1
+      ;;
+    : )
+      echo "Invalid option: $OPTARG requires an argument" 1>&2
+      exit 1
+      ;;
+  esac
+done
+shift $((OPTIND -1))
+
+files=("$@")
+
+if [[ -z "$old_key_id" ]]; then
+  echo "Error: -o old-key-id is required" >&2
+  exit 1
 fi
 
-if [ $# -lt 3 ]; then
-    usage
+if [[ ${#new_key_ids[@]} -eq 0 ]]; then
+  echo "Error: At least one -n new-key-id is required" >&2
+  exit 1
 fi
 
-OLD_KEY_ID=$1
-NEW_KEY_ID=$2
-shift 2
+if [[ ${#files[@]} -eq 0 ]]; then
+  echo "Error: At least one file is required" >&2
+  exit 1
+fi
 
-# check if keys and subkeys exist
-OLDKEY="$(gpg --list-secret-keys --with-colons $OLD_KEY_ID)"
-OLDSSB="$(echo "$OLDKEY" | egrep '^ssb' | cut -d: -f5)"
-OLDSEC="$(echo "$OLDKEY" | egrep '^sec' | cut -d: -f5)"
+# check if keys and subkeys exist for the old key
+oldkey="$(gpg --list-secret-keys --with-colons $old_key_id)"
+oldssb="$(echo "$oldkey" | egrep '^ssb' | cut -d: -f5)"
+oldsec="$(echo "$oldkey" | egrep '^sec' | cut -d: -f5)"
 
-# echo "OLDSEC: $OLDSEC"
-# echo "OLDSSB: $OLDSSB"
-
-if [ -z "$OLDSSB" ] || [ -z "$OLDSEC" ]; then
+if [ -z "$oldssb" ] || [ -z "$oldsec" ]; then
 	echo "Error: old key not found"
 	exit 1
 fi
 
-NEWKEY="$(gpg --list-secret-keys --with-colons $NEW_KEY_ID)"
-NEWSSB="$(echo "$NEWKEY" | egrep '^ssb' | cut -d: -f5)"
-NEWSEC="$(echo "$NEWKEY" | egrep '^sec' | cut -d: -f5)"
+# check that we have at least one key in the new set that we can decrypt with
+recipientargs="--recipient $old_key_id"
+found_private_key=0
+for new_key_id in "${new_key_ids[@]}"; do
+    recipientargs="${recipientargs} --recipient $new_key_id"
 
-# echo "NEWSEC: $NEWSEC"
-# echo "NEWSSB: $NEWSSB"
+    newkey="$(gpg --list-secret-keys --with-colons $new_key_id)"
+    newssb="$(echo "$newkey" | egrep '^ssb' | cut -d: -f5)"
+    newsec="$(echo "$newkey" | egrep '^sec' | cut -d: -f5)"
 
-if [ -z "$NEWSSB" ] || [ -z "$NEWSEC" ]; then
-	echo "Error: new key not found"
+    if [ -n "$newssb" ] && [ -n "$newsec" ]; then
+	found_private_key=1
+    fi
+done
+if [ $found_private_key -eq 0 ]; then
+	echo "Error: no private key found for decryption"
 	exit 1
 fi
 
-for file in "$@"; do
+for file in "${files[@]}"; do
     if [ ! -f $file ]; then
 	echo "File not found: $file"
 	continue
     fi
-    if file $file | grep -q "encrypted session key" && gpg --pinentry-mode cancel --list-packets $file 2>/dev/null | grep -q $OLDSSB; then
+    if file $file | grep -q "encrypted session key" && gpg --pinentry-mode cancel --list-packets $file 2>/dev/null | grep -q $oldssb; then
 	echo "Re-encrypting $file"
-	if [ -n "$DRY_RUN" ]; then
+	if [ $dry_run -eq 1 ]; then
+		echo "gpg --batch --yes --output $tmpfile --decrypt $file && gpg --batch --yes ${recipientargs} --encrypt --output $file $tmpfile"
 		continue
 	fi
 	tmpfile=$(mktemp)
 	chmod 600 $tmpfile
 	gpg --batch --yes --output $tmpfile --decrypt $file && \
-	    gpg --batch --yes --recipient $NEW_KEY_ID --recipient $OLD_KEY_ID --encrypt --output $file $tmpfile
+	    gpg --batch --yes ${recipientargs} --encrypt --output $file $tmpfile
 	rm $tmpfile
 	echo
     else
-	echo "Skipping $file (not encrypted with $OLD_KEY_ID)"
+	echo "Skipping $file (not encrypted with $old_key_id)"
     fi
 done
